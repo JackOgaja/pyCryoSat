5e69afd9b06184b0ea0f2e1cc8aec9be3b609b01
Jack Ogaja <12779448+Jaecq@users.noreply.github.com>
2018-08-12 17:52:05 -0400

    updates on the licenses

--- src/csarray.c
+++ src/csarray.c
@@ -1,3 +1,24 @@
+/* Copyright (C) 2018 Jack Ogaja.
+*
+*  Permission is hereby granted, free of charge, to any person obtaining a copy
+*  of this software and associated documentation files (the "Software"), to deal
+*  in the Software without restriction, including without limitation the rights
+*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*  copies of the Software, and to permit persons to whom the Software is
+*  furnished to do so, subject to the following conditions:
+*
+*  The above copyright notice and this permission notice shall be included in all
+*  copies or substantial portions of the Software.
+*
+*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*  SOFTWARE.
+*/
+
 /*--functions for reading ESA's CryoSat2 data sets--*/
 
 /*==============
@@ -220,11 +241,6 @@ csarray( t_cs_filehandle fH, long int n_records )
 
     if( fDataL2 != NULL )
     {
-       /*
-       Debug
-       */
-       //printf( "L2 day [0] = %"PRId32"\n", fDataL2[ 0 ].j_Day );
-       //printf( "L2 day [1] = %"PRId32"\n", fDataL2[ 1 ].j_Day );
 
        return fDataL2;
 
